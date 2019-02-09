869fba971633f6aa3b787f755281e6e7aee3ceb1
Niraj-Fonseka <fonseka.live@utexas.edu>
2019-02-08 23:01:37 -0600

    Adding some change to invoke the action

--- GithubActions.go
+++ GithubActions.go
@@ -9,5 +9,7 @@ func main() {
 	numberOne := 10
 	numberTwo := 20
 
+	fmt.Println("About to calculate number")
+
 	fmt.Printf("Number One : %d + Number Two : %d is %d \n", numberOne, numberTwo, numberOne+numberTwo)
 }
