cf836fed1141b7e96e663f155c55a72208e873f3
Niraj-Fonseka <fonseka.live@utexas.edu>
2019-02-10 10:29:06 -0600

    Adding new route

---
+++ GithubActionsServer.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Saying Hello world")
+		fmt.Fprintf(w, "HelloWorld")
+	})
+
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Saying test ")
+		fmt.Fprintf(w, "Test Endpoint again and again")
+	})
+
+	http.HandleFunc("/test_two", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Saying test two")
+		fmt.Fprintf(w, "Test Endpoint Two")
+	})
+
+	fmt.Println("Running App : ")
+	http.ListenAndServe(":8080", nil)
+}
---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "#### ---- Building the docker image ---- ####"
+docker build -t githubactions .
+
+echo "#### ---- Tagging the docker image ---- ####"
+docker tag githubactions gcr.io/nirajfonseka-prod/githubactions:latest
+
+echo "#### ---- Pushing the docker image to the docker registry ---- ####"
+docker push gcr.io/nirajfonseka-prod/githubactions
+
+echo "#### ---- Resetting the deployment image ---- ####"
+kubectl -n deployment set image deployment/githubactions githubactions-sha256=gcr.io/nirajfonseka-prod/githubactions:latest
+
+kubectl -n deployment set image deployment/githubactions githubactions-sha256=gcr.io/nirajfonseka-prod/githubactions
