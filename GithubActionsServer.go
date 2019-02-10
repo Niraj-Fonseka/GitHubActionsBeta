36e7a031a8931875ec3b96f17c9808ecd98f043a
Niraj-Fonseka <fonseka.live@utexas.edu>
2019-02-10 16:26:23 -0600

    Adding 5th route

--- GithubActionsServer.go
+++ GithubActionsServer.go
@@ -26,6 +26,11 @@ func main() {
 		fmt.Fprintf(w, "Test Three")
 	})
 
+	http.HandleFunc("/test_five", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Saying test two")
+		fmt.Fprintf(w, "Test Five")
+	})
+
 	fmt.Println("Running App : ")
 	http.ListenAndServe(":8080", nil)
 }
