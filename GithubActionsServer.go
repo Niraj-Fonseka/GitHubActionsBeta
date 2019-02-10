c409bbbef922c5c673065ecb3015eefd55011311
Niraj-Fonseka <fonseka.live@utexas.edu>
2019-02-10 13:47:28 -0600

    Invoke gcloud

--- GithubActionsServer.go
+++ GithubActionsServer.go
@@ -16,6 +16,11 @@ func main() {
 		fmt.Fprintf(w, "Test Two ")
 	})
 
+	http.HandleFunc("/test_three", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Saying test two")
+		fmt.Fprintf(w, "Test Three")
+	})
+
 	fmt.Println("Running App : ")
 	http.ListenAndServe(":8080", nil)
 }
