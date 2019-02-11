ab50bb32256e9c74bb2f337fb5855872a708c9ef
Niraj-Fonseka <fonseka.live@utexas.edu>
2019-02-10 20:44:09 -0600

    Adding time endpoint

--- GithubActionsServer.go
+++ GithubActionsServer.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -14,6 +15,11 @@ func main() {
 	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Saying Hello world")
 		fmt.Fprintf(w, "try /hello")
+	})
+
+	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Saying Hello world")
+		fmt.Fprintf(w, time.Now().UTC().String())
 	})
 
 	fmt.Println("Running App : ")
