aaac7b5b8cfb90bf93552b04bc127f0476f33ae1
Niraj-Fonseka <fonseka.live@utexas.edu>
2019-02-10 20:05:46 -0600

    Adding hi

--- GithubActionsServer.go
+++ GithubActionsServer.go
@@ -11,6 +11,11 @@ func main() {
 		fmt.Fprintf(w, "Hello World from GithubActions")
 	})
 
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Saying Hello world")
+		fmt.Fprintf(w, "try /hello")
+	})
+
 	fmt.Println("Running App : ")
 	http.ListenAndServe(":8080", nil)
 }
