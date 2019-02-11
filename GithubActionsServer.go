b85ed196349bacc47dce73d02c8141f03b01f8fc
Niraj-Fonseka <fonseka.live@utexas.edu>
2019-02-10 19:34:36 -0600

    Removing all the extra endpoints

--- GithubActionsServer.go
+++ GithubActionsServer.go
@@ -8,32 +8,7 @@ import (
 func main() {
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Saying Hello world")
-		fmt.Fprintf(w, "HelloWorld")
-	})
-
-	http.HandleFunc("/test_two", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Saying test two")
-		fmt.Fprintf(w, "Test Two !")
-	})
-
-	http.HandleFunc("/test_three", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Saying test two")
-		fmt.Fprintf(w, "Test Three")
-	})
-
-	http.HandleFunc("/test_four", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Saying test two")
-		fmt.Fprintf(w, "Test Three")
-	})
-
-	http.HandleFunc("/test_five", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Saying test two")
-		fmt.Fprintf(w, "Test Five")
-	})
-
-	http.HandleFunc("/test_five_test", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Saying test two")
-		fmt.Fprintf(w, "Test Five test")
+		fmt.Fprintf(w, "Hello World from GithubActions")
 	})
 
 	fmt.Println("Running App : ")
