339050798fec77ec135fec77c21d57c4bc136331
Niraj-Fonseka <fonseka.live@utexas.edu>
2019-02-12 19:25:21 -0600

    Adding the github actions route

--- GithubActionsServer.go
+++ GithubActionsServer.go
@@ -3,25 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
 )
 
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Saying Hello world")
-		fmt.Fprintf(w, "Hello World from GithubActions")
+	http.HandleFunc("/githubactions", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello From GitHub Actions")
 	})
 
-	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Saying hi")
-		fmt.Fprintf(w, "try /hello")
-	})
-
-	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Showing Time")
-		fmt.Fprintf(w, time.Now().UTC().String())
-	})
-
-	fmt.Println("Running App : 8080")
 	http.ListenAndServe(":8080", nil)
 }
